eac4fcc328508e8b6a5963df79def6516f9f4b92
Da0nn <166224732+CatIsApple@users.noreply.github.com>
2026-09-02 22:44:53 +0900

    feat: 머지 정렬 풀이 작성
    Closes #22

--- week2/2. advanced/02_merge_sort.py
+++ week2/2. advanced/02_merge_sort.py
@@ -16,13 +16,9 @@
 입력: [38, 27, 43, 3, 9, 82, 10]
 출력: [3, 9, 10, 27, 38, 43, 82]
 
-힌트:
-- 배열을 절반으로 분할 (재귀)
-- 각 부분을 재귀적으로 정렬
-- 정렬된 두 부분을 병합
 """
 
-def merge(arr, left, mid, right):
+def merge(left, right):
     """
     두 개의 정렬된 부분 배열을 병합하는 함수
     
@@ -32,50 +28,30 @@ def merge(arr, left, mid, right):
         mid: 왼쪽 부분의 끝 인덱스
         right: 오른쪽 부분의 끝 인덱스
     """
-    # TODO: 왼쪽과 오른쪽 부분 배열을 임시 배열로 복사
-    pass
-    
-    # TODO: 두 배열을 병합
-    pass
-    
-    
-    # TODO: left_arr와 right_arr를 비교하며 작은 값을 arr에 복사
-    pass
-    
-    # TODO: 남은 원소들을 복사
-    # left_arr에 남은 원소가 있으면 복사
-    # right_arr에 남은 원소가 있으면 복사
-    pass
+    result:list = []
+    i, j = 0, 0
 
-def merge_sort_helper(arr, left, right):
-    """
-    머지 정렬 재귀 함수
-    
-    Args:
-        arr: 배열
-        left: 시작 인덱스
-        right: 끝 인덱스
-    """
-    # TODO: base case - left가 right보다 작을 때만 정렬
-    ## 중간 지점 계산
-    ## 왼쪽 절반 재귀 정렬
-    ## 오른쪽 절반 재귀 정렬
-    ## 정렬된 두 절반을 병합
-    pass
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
 
 def merge_sort(arr):
-    """
-    머지 정렬 메인 함수
-    
-    Args:
-        arr: 정렬할 배열
-    
-    Returns:
-        정렬된 배열
-    """
-    if len(arr) > 1:
-        merge_sort_helper(arr, 0, len(arr) - 1)
-    return arr
+    if len(arr) <= 1:
+        return arr
+
+    mid: int = len(arr) // 2
+    left: list = arr[:mid]
+    right: list = arr[mid:]
+
+    return merge(merge_sort(left), merge_sort(right))
 
 # 테스트 케이스
 if __name__ == "__main__":
@@ -109,5 +85,4 @@ if __name__ == "__main__":
     print(f"정렬 전: {arr4}")
     result4 = merge_sort(arr4.copy())
     print(f"정렬 후: {result4}")
-
 
